5ff72ffab512bab95aebc3c48bb3084066c567b7
AllenJoeG <allen.joeg@gmail.com>
2023-06-13 13:52:18 -0500

    rolled up errors from auth

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,6 +1,26 @@
+import { getUserByClerkID } from "@/utils/auth"
+import { prisma } from "@/utils/db"
+
+
 //This page will fetch all journals by date, organizing them, and contain new journal button & functionality
 
-const JournalPage = () => {
+const getEntries = async () => {
+  const user = await getUserByClerkID()
+  const entries = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
+  return entries
+}
+
+const JournalPage = async () => {
+  const entries = await getEntries()
+  console.log('entries ', entries)
 
   return(
     <div>
---
+++ utils/auth.ts
@@ -0,0 +1,16 @@
+//To fetch the user ID of the user in the database given the Clerk ID. Helper function!
+
+import { auth } from "@clerk/nextjs"
+import { prisma } from "./db"
+
+export const getUserByClerkID = async () => {
+  const { userId } = await auth()
+
+  const user = await prisma.user.findUniqueOrThrow({
+    where: {
+      clerkId: userId,
+    },
+  })
+
+  return user
+}
