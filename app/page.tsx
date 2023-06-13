9561a730400b71111603e172b751997d362a1c80
AllenJoeG <allen.joeg@gmail.com>
2023-06-13 12:54:20 -0500

    dynamic HREF routing in root app page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { auth } from '@clerk/nextjs'
 
-export default function Home() {
+//check for user
+
+//Server component, not client, so it can be async!!
+export default async function Home() {
+
+  const {userId} = await auth()
+
+  let href = userId ? '/journal' : '/new-user'
+
+
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       {/* square brackets tells Tailwind to use a custom value. mx-auto sets margins centered */}
@@ -10,7 +20,7 @@ export default function Home() {
         {/* text size, text color opacity */}
         <p className="text=2xl text-white/70 mb-4"> Use this app to track your daily experiences and let Chat GPT read your diary. Be authentic. Honest.</p>
         <div>
-          <Link href="/journal">
+          <Link href={href}>
             <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">get started</button>
           </Link>
         </div>
